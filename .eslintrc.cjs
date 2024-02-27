f2225ac707b22c9e89e5f6af1d44c34bb380acca
Yasser Al-Najjar <yasseralnajjar@outlook.sa>
2024-02-27 17:53:08 +0200

    change tecnology

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+const path = require('node:path');
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
+  plugins: ['react', 'react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      {
+        allowConstantExport: true,
+      },
+    ],
+    'no-unused-vars': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-import-assign': ['off'],
+    'react/prop-types': [
+      'off',
+      {
+        ignore: null,
+        customValidators: null,
+        skipUndeclared: null,
+      },
+    ],
+    'react/jsx-props-no-spreading': ['off'],
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'auto',
+  printWidth: 80,
+};
+
+module.exports = config;
